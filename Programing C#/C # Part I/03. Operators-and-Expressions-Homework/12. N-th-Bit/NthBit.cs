d27ff0ebfb9f42003d153b394187b93bc528d535
dushka-dragoeva <d.dragoeva@abv.bg>
2016-04-01 15:00:24 +0300

    NthBit.cs - changed data types and logic

--- Programing C#/C # Part I/03. Operators-and-Expressions-Homework/12. N-th-Bit/NthBit.cs
+++ Programing C#/C # Part I/03. Operators-and-Expressions-Homework/12. N-th-Bit/NthBit.cs
@@ -6,13 +6,12 @@ public class NthBit
 {
     public static void Main()
     {
-        long number = long.Parse(Console.ReadLine());
+        uint number = uint.Parse(Console.ReadLine());
         int position = int.Parse(Console.ReadLine());
 
-        int mask = 1 << position;
-        long numberAndMask = number & mask;
-        long bit = numberAndMask >> position;
+        uint mask = 1;
 
-        Console.WriteLine(bit);
+        var output = ((mask << position & number) == 0) ? "0" : "1";
+        Console.WriteLine(output);
     }
 }
