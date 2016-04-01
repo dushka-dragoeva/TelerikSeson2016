4e2ef627f3d7d6b9613f82f97212dcde2ae78ffc
dushka-dragoeva <d.dragoeva@abv.bg>
2016-04-01 09:51:23 +0300

    Renamed to Loop-homework, refactred and optimised task 04.

--- Programing C#/C # Part I/06. Loops/04. Print -A-Deck/PrintADeck.cs
+++ Programing C#/C # Part I/06. Loops/04. Print -A-Deck/PrintADeck.cs
@@ -1,61 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*Write a program that reads a card sign(as a char) from the console and generates and prints all possible
+cards from a standard deck of 52 cards up to the card with the given sign(without the jokers). The cards 
+should be printed using the classical notation (like 5 of spades, A of hearts, 9 of clubs; and K of diamonds).
+The card faces should start from 2 to A.
+Print each card face in its four possible suits: clubs, diamonds, hearts and spades.*/
+using System;
 
-class PrintADeck
+public class PrintADeck
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        string input = Console.ReadLine();
-        int cardSign = 0;
+        string cardFace = Console.ReadLine();
         int length = 0;
 
-        bool isNumber = int.TryParse(input, out cardSign);
-
-        if (isNumber)
+        switch (cardFace)
         {
-            length = cardSign;
-        }
-        else
-        {
-            char cardFace = input.ToCharArray()[0];
-            switch (cardFace)
-            {
-                case 'J':
-                    length = 11;
-                    break;
-                case 'D':
-                    length = 12;
-                    break;
-                case 'K':
-                    length = 13;
-                    break;
-                case 'A':
-                    length = 14;
-                    break;
-            }
-        }
-        string output = "{0} of spades, {0} of clubs, {0} of hearts, {0} of diamonds";
-
-        if (length <= 10)
-        {
-            for (int i = 2; i <= length; i++)
-            {
-                Console.WriteLine(output, i);
-            }
+            case "J":
+                length = 11;
+                break;
+            case "D":
+                length = 12;
+                break;
+            case "K":
+                length = 13;
+                break;
+            case "A":
+                length = 14;
+                break;
+            default:
+                length = int.Parse(cardFace);
+                break;
         }
 
-        else
+        for (int i = 2; i <= length; i++)
         {
-            for (int i = 2; i <= 10; i++)
+            cardFace = string.Empty;
+            switch (i)
             {
-                Console.WriteLine(output, i);
+                case 11:
+                    cardFace = "J";
+                    break;
+                case 12:
+                    cardFace = "Q";
+                    break;
+                case 13:
+                    cardFace = "K";
+                    break;
+                case 14:
+                    cardFace = "A";
+                    break;
+                default:
+                    cardFace = string.Empty + i;
+                    break;
             }
 
-          
+            string output = "{0} of spades, {0} of clubs, {0} of hearts, {0} of diamonds";
+            Console.WriteLine(output, cardFace);
         }
     }
 }
-
---
+++ Programing C#/C # Part I/06. Loops/05. Calculate!/Calculate.cs
@@ -0,0 +1,13 @@
+﻿/// Write a program that, for a given two integer numbers N and x, calculates the sum 
+/// S = 1 + 1!/x + 2!/x<sup>2</sup> + … + n!/x<sup>N</sup>.Use only one loop.
+/// Print the result with 5 digits after the decimal point.
+
+using System;
+
+ public   class Calculate
+    {
+        static void Main()
+        {
+        }
+    }
+
