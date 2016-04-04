241a04d2ac69b05db0b1af76c41213b1e02a1b10
dushka-dragoeva <d.dragoeva@abv.bg>
2016-04-04 16:55:32 +0300

    Added Exam Solutions - C#1 2015 Feb 02 Morning tasks 1,2,3

---
+++ Exam Preparations/C-Sharp-Part-One/TaskOne2015Feb2Morning/Printing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    public const int SheetOfPapersInRealm = 500;
+
+    static void Main(string[] args)
+    {
+        int NumberOfStudents = int.Parse(Console.ReadLine());
+        int NumberOfsheets = int.Parse(Console.ReadLine());
+        decimal PriceOfRealmPaper = decimal.Parse(Console.ReadLine());
+
+        decimal moneySaved = NumberOfStudents * NumberOfsheets * (PriceOfRealmPaper / SheetOfPapersInRealm);
+        Console.WriteLine("{0:F2}", moneySaved);
+    }
+}
---
+++ Exam Preparations/C-Sharp-Part-One/TaskThree2015Feb2MorningSaddyKopper/SaddyKopper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+public class SaddyKopper
+{
+
+    public BigInteger Transformation(BigInteger number, SaddyKopper curr)
+    {
+        BigInteger product = 1;
+        int sum = 0;
+        BigInteger num = number / 10;
+
+
+        for (int i = 0; i < number.ToString().Length - 1; i++)
+        {
+            sum = curr.SumOfDigitsAtEvenPosition(num);
+            product *= sum;
+            num = num / 10;
+        //  sum = 0;
+        }
+
+        return product;
+    }
+
+    public int SumOfDigitsAtEvenPosition(BigInteger number)
+    {
+        int sum = 0;
+        int[] numberAsArray = Array.ConvertAll(number.ToString().ToArray(), x => (int)x - 48);
+
+        for (int i = 0; i < numberAsArray.Length; i=i+2)
+        {
+
+            sum += numberAsArray[i];
+            //  if (i % 2 == 0)
+            //  {
+            //      sum += numberAsArray[i];
+            // }
+        }
+
+        return sum;
+    }
+    public static void Main()
+    {
+        BigInteger numberM = BigInteger.Parse(Console.ReadLine());
+
+        SaddyKopper current = new SaddyKopper();
+        BigInteger product = 1;
+
+        int transformers = 0;
+
+
+        while (numberM.ToString().Length > 1 && transformers < 10)
+        {
+            product = current.Transformation(numberM, current);
+            numberM = product;
+            transformers++;
+        }
+
+
+        if (transformers < 10)
+        {
+            Console.WriteLine(transformers);
+            Console.WriteLine(numberM);
+        }
+
+        else
+        {
+            Console.WriteLine(numberM);
+        }
+    }
+}
+
+
+
