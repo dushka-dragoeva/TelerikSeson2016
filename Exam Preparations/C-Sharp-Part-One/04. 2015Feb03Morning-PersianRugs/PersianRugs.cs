439cec01743c882d79f8f90824ddd09ff8456031
dushka-dragoeva <d.dragoeva@abv.bg>
2016-04-07 15:54:16 +0300

    Exam Preparations/C-Sharp-Part-One/04. 2015Feb03Morning-PersianRugs - start work

---
+++ Exam Preparations/C-Sharp-Part-One/04. 2015Feb03Morning-PersianRugs/PersianRugs.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public class PersianRugs
+{
+    public static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+        int distanseSmallerTriangle = int.Parse(Console.ReadLine());
+        int rugSize = (2 * size) + 1;
+
+        char[,] rug = new char[rugSize, rugSize];
+
+        for (int row = 0; row < rugSize; row++)
+        {
+            for (int col = 0; col < rugSize; col++)
+            {
+                if (row >= 0 && row < size)
+                {
+                    if (col == row)
+                    {
+                        rug[row, col] = '\\';
+                    }
+                    else if (col == row + size)
+                    {
+                        rug[row, col] = '/';
+                    }
+                    else if (row > 0 && ((col > 0 && col < row) 
+                        || col > rugSize - row ))
+                    {
+                        rug[row, col] = '#';
+                    }
+                    else
+                    {
+                        rug[row, col] = '*';
+                    }
+                }
+
+                if (row == size)
+                {
+                    if (col == size)
+                    {
+                        rug[row, col] = 'X';
+                    }
+                    else
+                    {
+                        rug[row, col] = '#';
+                    }
+                }
+                //else
+                //{
+                //    rug[row, col] = '*';
+                //}
+            }
+        }
+
+        for (int i = 0; i < rugSize; i++)
+        {
+            for (int j = 0; j < rugSize; j++)
+            {
+                Console.Write(rug[i, j]);
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
+
