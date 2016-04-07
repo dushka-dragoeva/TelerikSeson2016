8ae6c9910ca3c4b7b1bee7dc5ca4fcb09eae7bbd
dushka-dragoeva <d.dragoeva@abv.bg>
2016-04-07 15:16:04 +0300

    Exam Preparations/C-Sharp-Part-One/2015Feb02Evening added my solutions for CubeThreeD and SequemceOfBits

---
+++ Exam Preparations/C-Sharp-Part-One/04. 2015Feb02Evening-CubeThreeD/CubeThreeD.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+public class CubeThreeD
+{
+    public static void Main()
+    {
+        int cubeSize = int.Parse(Console.ReadLine());
+
+        int matrixSize = (2 * cubeSize) - 1;
+        char[,] cube = new char[matrixSize, matrixSize];
+
+        for (int row = 0; row < matrixSize; row++)
+        {
+            for (int col = 0; col < matrixSize; col++)
+            {
+                if (row == 0 && col < cubeSize)
+                {
+                    cube[row, col] = ':';
+                }
+                else if (row > 0 && row < cubeSize - 1)
+                {
+                    if (col == 0 || col == cubeSize - 1 || col == cubeSize + row - 1)
+                    {
+                        cube[row, col] = ':';
+                    }
+                    else if (col >= cubeSize && col < cubeSize + row - 1)
+                    {
+                        cube[row, col] = '|';
+                    }
+                    else
+                    {
+                        cube[row, col] = ' ';
+                    }
+                }
+                else if (row == cubeSize - 1)
+                {
+                    if (col >= cubeSize && col < cubeSize + row - 1)
+                    {
+                        cube[row, col] = '|';
+                    }
+                    else
+                    {
+                        cube[row, col] = ':';
+                    }
+                }
+                else if (row >= cubeSize && row < matrixSize - 1)
+                {
+                    if (col == row - cubeSize + 1 || col == row || col == matrixSize - 1)
+                    {
+                        cube[row, col] = ':';
+                    }
+                                        else if (col > ((row - cubeSize) + 1) && col < row)
+                    {
+                        cube[row, col] = '-';
+                    }
+                    else if (col > row && col < matrixSize - 1)
+                    {
+                        cube[row, col] = '|';
+                    }
+                    else
+                    {
+                        cube[row, col] = ' ';
+                    }
+                }
+                else if (row == matrixSize - 1 && col >= cubeSize - 1)
+                {
+                    cube[row, col] = ':';
+                }
+                else
+                {
+                    cube[row, col] = ' ';
+                }
+            }
+        }
+
+        for (int i = 0; i < matrixSize; i++)
+        {
+            for (int j = 0; j < matrixSize; j++)
+            {
+                Console.Write(cube[i, j]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Exam Preparations/C-Sharp-Part-One/05. 2015Feb02Evening-SequenceOfBits/SequenceOfBits.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class SequenceOfBits
+
+{
+    public static void Main()
+    {
+        int lengthOgNumbers = int.Parse(Console.ReadLine());
+
+        int maxSequenceOfZeros = 0;
+        int currentSequenceOfZeros = 0;
+        int maxSequenceOfOnes = 0;
+        int currentSequenceOfOnes = 0;
+        char lastBit = '?';
+
+        for (int i = 0; i < lengthOgNumbers; i++)
+        {
+            int currentNumber = int.Parse(Console.ReadLine());
+            string binaryNumber = Convert.ToString(currentNumber, 2).PadLeft(30, '0');
+
+            foreach (var bit in binaryNumber)
+            {
+                if (bit == '0')
+                {
+                    if (lastBit == '0')
+                    {
+                        currentSequenceOfZeros++;
+                    }
+                    else
+                    {
+                        currentSequenceOfZeros = 1;
+                    }
+
+                    lastBit = bit;
+                    maxSequenceOfZeros = Math.Max(maxSequenceOfZeros, currentSequenceOfZeros);
+                }
+                else
+                {
+                    if (lastBit == '1')
+                    {
+                        currentSequenceOfOnes++;
+                    }
+                    else
+                    {
+                        currentSequenceOfOnes = 1;
+                    }
+
+                    lastBit = bit;
+                    maxSequenceOfOnes = Math.Max(maxSequenceOfOnes, currentSequenceOfOnes);
+                }
+            }
+        }
+
+        Console.WriteLine(maxSequenceOfOnes);
+        Console.WriteLine(maxSequenceOfZeros);
+    }
+}
