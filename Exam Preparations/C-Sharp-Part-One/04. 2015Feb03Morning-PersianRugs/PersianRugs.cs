7b4b427fff75cf127f0f2af51fc3439fe7f355c4
dushka-dragoeva <d.dragoeva@abv.bg>
2016-04-08 07:30:58 +0300

    Exam Preparations/C-Sharp-Part-One/04.2015Feb03Morning-PersianRugs - solution is ready

--- Exam Preparations/C-Sharp-Part-One/04. 2015Feb03Morning-PersianRugs/PersianRugs.cs
+++ Exam Preparations/C-Sharp-Part-One/04. 2015Feb03Morning-PersianRugs/PersianRugs.cs
@@ -5,7 +5,7 @@ public class PersianRugs
     public static void Main()
     {
         int size = int.Parse(Console.ReadLine());
-        int distanseSmallerTriangle = int.Parse(Console.ReadLine());
+        int distance = int.Parse(Console.ReadLine());
         int rugSize = (2 * size) + 1;
 
         char[,] rug = new char[rugSize, rugSize];
@@ -14,42 +14,40 @@ public class PersianRugs
         {
             for (int col = 0; col < rugSize; col++)
             {
-                if (row >= 0 && row < size)
+                if (row == size && (col == size))
                 {
-                    if (col == row)
-                    {
-                        rug[row, col] = '\\';
-                    }
-                    else if (col == row + size)
-                    {
-                        rug[row, col] = '/';
-                    }
-                    else if (row > 0 && ((col > 0 && col < row) 
-                        || col > rugSize - row ))
-                    {
-                        rug[row, col] = '#';
-                    }
-                    else
-                    {
-                        rug[row, col] = '*';
-                    }
+                    rug[row, col] = 'X';
+                }
+                else if ((col == row)
+                    || (row < size - distance - 1 && col == row + distance + 1)
+                    || (row > size + distance + 1 && col == row - distance - 1))
+                {
+                    rug[row, col] = '\\';
+                }
+                else if ((col == rugSize - row - 1)
+                     || (row < size - distance - 1 && col == rugSize - row - distance - 2)
+                     || (row > size + distance + 1 && col == rugSize - row + distance)
+                    )
+                {
+                    rug[row, col] = '/';
                 }
 
-                if (row == size)
+                else if ((row < size - distance - 1 && col > row + distance + 1 && col < rugSize - row - distance - 2)
+                    || (row > size + distance + 1 && col < row - distance - 1 && col > rugSize - row + distance)
+                    )
+
                 {
-                    if (col == size)
-                    {
-                        rug[row, col] = 'X';
-                    }
-                    else
-                    {
-                        rug[row, col] = '#';
-                    }
+                    rug[row, col] = '.';
                 }
-                //else
-                //{
-                //    rug[row, col] = '*';
-                //}
+                else if ((row <= size && ((col >= 0 && col < row) || col >= rugSize - row))
+    || (row > size && ((col >= 0 && col < rugSize - row) || col > row)))
+                {
+                    rug[row, col] = '#';
+                }
+                else
+                {
+                    rug[row, col] = ' ';
+                }
             }
         }
 
