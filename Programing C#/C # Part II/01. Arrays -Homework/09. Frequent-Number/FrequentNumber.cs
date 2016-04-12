a29e7ff43f1b865adbfa5f8343d4ed6a913cebea
Dushka <d.dragoeva@abv.bg>
2016-04-12 07:59:36 +0300

    Programing C#/C # Part II/01. Arrays -Homework - problems 9 and 10 added

---
+++ Programing C#/C # Part II/01. Arrays -Homework/09. Frequent-Number/FrequentNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class FrequentNumber
+{
+    public static void Main(string[] args)
+    {
+        int length = int.Parse(Console.ReadLine());
+        int[] arr = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(arr);
+        int maxCounter = 1;
+        int tempCounter = 1;
+        int mostFrequentNumber = 0;
+
+        for (int i = 0; i < length - 1; i++)
+        {
+            if (arr[i] == arr[i + 1])
+            {
+                tempCounter++;
+            }
+            else
+            {
+                if (tempCounter > maxCounter)
+                {
+                    maxCounter = tempCounter;
+                    mostFrequentNumber = arr[i];
+                    tempCounter = 1;
+                }
+                else
+                {
+                    tempCounter = 1;
+                }
+            }
+        }
+
+        string outputFormat = "{0} ({1} times)";
+        Console.WriteLine(outputFormat, mostFrequentNumber, maxCounter);
+    }
+}
---
+++ Programing C#/C # Part II/01. Arrays -Homework/10. Find-Sum-In-Array/FindSumInArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+public class FindSumInArray
+{
+    public static void Main(string[] args)
+    {
+        string input = "4, 3, 1, 4, 2, 5, 8";
+        int sum = 55;
+        int startIndex = 0;
+        int endIndex = 0;
+        int counter = 0;
+        bool isSumExist = false;
+
+        int[] arr = input.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        int tempSum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            tempSum += arr[i];
+
+            if (tempSum < sum)
+            {
+                counter++;
+            }
+            else if (tempSum > sum)
+            {
+                tempSum = 0;
+                endIndex = i;
+                startIndex = endIndex - counter;
+                i = startIndex;
+                counter = 0;
+            }
+            else if (tempSum == sum)
+            {
+                endIndex = i;
+                startIndex = endIndex - counter;
+                isSumExist = true;
+                break;
+            }
+        }
+
+        if (isSumExist)
+        {
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                Console.Write(arr[i]);
+                if (i < endIndex)
+                {
+                    Console.Write(',');
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Sum doesn't exist.");
+        }
+    }
+}
