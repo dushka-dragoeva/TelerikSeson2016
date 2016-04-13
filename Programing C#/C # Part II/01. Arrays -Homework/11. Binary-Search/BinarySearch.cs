2eab6f65d10576a4ffb1718a665b6d96925f1a7e
Dushka <d.dragoeva@abv.bg>
2016-04-13 07:19:21 +0300

    Programing C#/C # Part II/01. Arrays -Homework/11. IndexOfLetters added

---
+++ Programing C#/C # Part II/01. Arrays -Homework/11. Binary-Search/BinarySearch.cs
@@ -0,0 +1,48 @@
+﻿/// Write a prograxm that finds the index of given element X in a sorted array of N integers by using the 
+/// Binary search algorithm.
+using System;
+
+public class BinarySearch
+{
+    public static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        int numberX = int.Parse(Console.ReadLine());
+
+        int counter = 0;
+        int startIndex = 0;
+        int endIndex = length - 1;
+        int midIndex = (endIndex - startIndex) / 2;
+        int result = -1;
+
+        while (counter <= length / 2)
+        {
+            if (arr[midIndex] == numberX)
+            {
+                result = midIndex;
+                break;
+            }
+            else if (arr[midIndex] < numberX)
+            {
+                startIndex = midIndex + 1;
+                midIndex = (endIndex + startIndex) / 2;
+                counter++;
+            }
+            else
+            {
+                endIndex = midIndex - 1;
+                midIndex = (startIndex + endIndex) / 2;
+                counter++;
+            }
+        }
+
+        Console.WriteLine(result);
+        Console.WriteLine(counter);
+    }
+}
---
+++ Programing C#/C # Part II/01. Arrays -Homework/12. Index-Of-Letters/IndexOfLetters.cs
@@ -0,0 +1,54 @@
+﻿/// Write a program that creates an alphabetay containing all letters from the alphabet (a-z). 
+/// Read a word from the console and print the index of each of its letters in the alphabetay.
+using System;
+
+public class Program
+{
+    public static int FindIndex(char[] alphabet, char letter)
+    {
+        int counter = 0;
+        int startIndex = 0;
+        int endIndex = alphabet.Length - 1;
+        int midIndex = (endIndex - startIndex) / 2;
+        int indexLetter = -1;
+
+        while (true)
+        {
+            if (alphabet[midIndex] == letter)
+            {
+                indexLetter = midIndex;
+                break;
+            }
+            else if (alphabet[midIndex] < letter)
+            {
+                startIndex = midIndex + 1;
+                midIndex = (endIndex + startIndex) / 2;
+                counter++;
+            }
+            else
+            {
+                endIndex = midIndex - 1;
+                midIndex = (startIndex + endIndex) / 2;
+                counter++;
+            }
+        }
+
+        return indexLetter;
+    }
+
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine().ToLower();
+
+        char[] alphabet = new char[26];
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            alphabet[i] = (char)(i + (int)'a');
+        }
+
+        foreach (var item in input)
+        {
+            Console.WriteLine(FindIndex(alphabet, item));
+        }
+    }
+}
