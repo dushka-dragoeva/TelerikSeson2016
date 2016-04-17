95b57af490ad49ce04673163f2c3c7664dadb153
Dushka <d.dragoeva@abv.bg>
2016-04-17 16:11:44 +0300

    Exam Preparations/ C-Sharp-Part-One/01. 2013Dec06Evening-TowFourEight, NightmareOnCodeStreet - added my

---
+++ Exam Preparations/C-Sharp-Part-One/01. 2013Dec06Evening-TowFourEight/TowFourEight.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class TowFourEight
+{
+    public static void Main()
+    {
+        long a = long.Parse(Console.ReadLine());
+        long sicretSymbolB = long.Parse(Console.ReadLine());
+        long c = long.Parse(Console.ReadLine());
+        long result = 0;
+
+        switch (sicretSymbolB)
+        {
+            case 2:
+                result = a % c;
+                break;
+            case 4:
+                result = a + c;
+                break;
+            case 8:
+                result = a * c;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        long finalResult = 0;
+        if (result % 4 == 0)
+        {
+            finalResult = result / 4;
+            Console.WriteLine(finalResult);
+        }
+        else
+        {
+            finalResult = result % 4;
+            Console.WriteLine(finalResult);
+        }
+
+        Console.WriteLine(result);
+    }
+}
---
+++ Exam Preparations/C-Sharp-Part-One/02.2013Dec06Evening-NigthmareOnCodeStreet/NightmareOnCodeStreet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class NightmareOnCodeStreet
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        int sum = 0;
+        int counter = 0;
+        int position = 0;
+
+        foreach (char symbol in text)
+        {
+            if (position % 2 == 1 && char.IsDigit(symbol))
+            {
+                counter++;
+                int digit = symbol - '0';
+                sum += digit;
+            }
+
+            position++;
+        }
+
+        ////for (int i = 0; i < text.Length; i++)
+        ////{
+        ////    if (i % 2 == 1&& char.IsDigit(text[i]))
+        ////    {
+        ////        counter++;
+        ////            int digit = (text[i] - '0');
+        ////            sum += digit;
+        ////    }
+        ////}
+        Console.WriteLine("{0} {1}", counter, sum);
+    }
+}
