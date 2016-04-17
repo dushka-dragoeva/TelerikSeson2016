cc0796b551355c1517ac49eec10dc244c395d6a0
Dushka <d.dragoeva@abv.bg>
2016-04-17 08:49:54 +0300

    Exam Preparations/C-Sharp-Part-One/05. 2015Feb03Morning-SearchinBits/SearchInBit.cs - my solution

---
+++ Exam Preparations/C-Sharp-Part-One/05. 2015Feb03Morning-SearchinBits/SearchInBit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class SearchInbit
+{
+    static void Main(string[] args)
+    {
+        int numberS = int.Parse(Console.ReadLine());
+        int length = int.Parse(Console.ReadLine());
+        int counter = 0;
+        int mask = 15;
+        int numberSandMask = numberS & mask;
+        int currentNumberAndMask = 1;
+
+        for (int i = 0; i < length; i++)
+        {
+            int currentNumber = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < ((30-4)+1); j++)
+            {
+                currentNumberAndMask = currentNumber & (mask << j);
+                
+                if (currentNumberAndMask == numberSandMask<<j)
+                {
+                    counter++;
+                }
+            }
+        }
+
+        Console.WriteLine(counter);
+    }
+}
