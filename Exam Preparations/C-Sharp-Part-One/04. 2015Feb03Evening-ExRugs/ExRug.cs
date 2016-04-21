7c9c141f83ac7982c4d068d8e3ec6dd52f5d07de
Dushka <d.dragoeva@abv.bg>
2016-04-21 13:52:15 +0300

    Exam Prep. C-Sharp-Part-One/04. 2015Feb03Evening-ExRugs start working on my solution (not finished)

---
+++ Exam Preparations/C-Sharp-Part-One/04. 2015Feb03Evening-ExRugs/ExRug.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class ExRug
+{
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+        int hashTag = int.Parse(Console.ReadLine());
+        int rugSize = (2 * size) + 1;
+
+        var srartIndex = 0;
+        var endIndex = rugSize - 1;
+        var index = hashTag / 2 + 1;
+
+        char[,] rug = new char[rugSize, rugSize];
+
+        for (int row = 0; row < rugSize; row++)
+        {
+            for (int col = 0; col < rugSize; col++)
+            {
+                if (size < hashTag)
+                {
+                    rug[row, col] = '#';
+                }
+                else
+                {
+                    if ((col < index + row && col > row - index) ||
+                        (col < rugSize - 1 - row + index && col > rugSize - 1 - index - row))
+
+                    {
+                        rug[row, col] = '#';
+                    }
+
+                    else if (((row < index || row > size) && col == index + row)
+                        ||(row >size-index-1 && col==row-index))
+                    {
+                        rug[row, col] = '\\';
+                    }
+                    else if (row > size - index - 1)
+                    {
+                        
+                    }
+                    else
+                    {
+                        rug[row, col] = '.';
+                    }
+                }
+
+
+
+            }
+        }
+
+        for (int i = 0; i < rugSize; i++)
+        {
+            for (int j = 0; j < rugSize; j++)
+            {
+                Console.Write(rug[i, j]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Exam Preparations/C-Sharp-Part-One/04. 2015Feb03Evening-ExRugs/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("04. 2015Feb03Evening-ExRugs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("04. 2015Feb03Evening-ExRugs")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("18981f4c-9e05-4802-a1f1-4cb06dc37795")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
