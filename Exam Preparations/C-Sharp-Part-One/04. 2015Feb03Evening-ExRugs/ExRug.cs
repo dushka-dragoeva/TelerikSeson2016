f4c17e49e45d5cd12acbe05fa0dca1c8c1ec9aff
Dushka <d.dragoeva@abv.bg>
2016-04-22 12:34:30 +0300

    Exam Preparation C-Sharp-Part-One/04. 2015Feb03Evening-ExRugs - final solution with formulas

--- Exam Preparations/C-Sharp-Part-One/04. 2015Feb03Evening-ExRugs/ExRug.cs
+++ Exam Preparations/C-Sharp-Part-One/04. 2015Feb03Evening-ExRugs/ExRug.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 
-class ExRug
+public class ExRug
 {
-    static void Main()
+    public static void Main()
     {
         int size = int.Parse(Console.ReadLine());
         int patternSize = int.Parse(Console.ReadLine());
         int rugSize = (2 * size) + 1;
 
         int middleRug = size;
-        var middlePattern = patternSize / 2 + 1;
+        var middlePattern = (patternSize / 2) + 1;
 
         char patternEdge = 'X';
         char leftDiagonal = '\\';
@@ -21,27 +21,32 @@ class ExRug
         {
             for (int col = 0; col < rugSize; col++)
             {
-                // print X
-                if ((row == middleRug - middlePattern || row == middleRug + middlePattern) && col == middleRug ||
-                   row == middleRug && (col == middleRug - middlePattern || col == middleRug + middlePattern))
+                /// print X
+                if (((row == middleRug - middlePattern || row == middleRug + middlePattern) && col == middleRug)
+                    || (row == middleRug && (col == middleRug - middlePattern || col == middleRug + middlePattern)))
                 {
                     Console.Write(patternEdge);
                 }
-                // ptinl left diagonals
-                else if (col - row ==  middlePattern && (row < middleRug - middlePattern || row > middleRug) ||
-                    row - col ==middlePattern && (row<middleRug||row>middleRug+middlePattern))
+
+                /// ptinl left diagonals
+                else if ((col - row == middlePattern && (row < middleRug - middlePattern || row > middleRug))
+                    || (row - col == middlePattern && (row < middleRug || row > middleRug + middlePattern)))
                 {
                     Console.Write(leftDiagonal);
                 }
-              //  print right diagonals
-               else if (col + row == rugSize - 1 - middlePattern && (row < middleRug - middlePattern || row > middleRug)
 
-                    || col + row == rugSize - 1 + middlePattern && (row < middleRug || row > middleRug + middlePattern))
+                /// print right diagonals
+                else if ((col + row == rugSize - 1 - middlePattern && (row < middleRug - middlePattern || row > middleRug))
+                     || (col + row == rugSize - 1 + middlePattern && (row < middleRug || row > middleRug + middlePattern)))
                 {
                     Console.Write(rightDiagonal);
                 }
-                else if ((row<middleRug-middlePattern && col-row>middlePattern &&col+row<rugSize-1-middlePattern)
-                    || row>middleRug+middlePattern)
+
+                /// print dots
+                else if ((row < middleRug - middlePattern && (col - row > middlePattern && col + row < rugSize - 1 - middlePattern))
+                    || (row > middleRug + middlePattern && row - col > middlePattern && row + col > rugSize - 1 + middlePattern)
+                    || (row + col < rugSize - 1 - middlePattern && row - col > middlePattern)
+                    || (row + col > rugSize - 1 + middlePattern && col - row > middlePattern))
                 {
                     Console.Write(dot);
                 }
@@ -49,7 +54,6 @@ class ExRug
                 {
                     Console.Write(pattern);
                 }
-
             }
 
             Console.WriteLine();
