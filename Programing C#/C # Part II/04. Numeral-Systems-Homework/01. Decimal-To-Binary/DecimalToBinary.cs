97da3582f9f688603b2a32ae9b979d5f900742ee
dushka-dragoeva <d.dragoeva@abv.bg>
2016-05-18 10:38:18 +0300

    Programing C#/C # Part II/04. Numeral-Systems-Homework/01. Decimal-To-Binary - added

---
+++ Programing C#/C # Part II/04. Numeral-Systems-Homework/01. Decimal-To-Binary/DecimalToBinary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+public class DecimalToBinary
+{
+    public static void Main(string[] args)
+    {
+        var num = long.Parse(Console.ReadLine());
+        var result = ConvertToBynary(num);
+        Console.WriteLine(result);
+    }
+
+    private static string ConvertToBynary(long number)
+    {
+        StringBuilder binary = new StringBuilder();
+        while (number > 0)
+        {
+            var digit = number % 2;
+            binary.Insert(0, digit);
+            number = number / 2;
+        }
+
+        return binary.ToString();
+    }
+}
