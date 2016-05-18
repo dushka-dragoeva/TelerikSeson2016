8479194c14622eaa8ee6f91051dd4f13d81dba9b
dushka-dragoeva <d.dragoeva@abv.bg>
2016-05-18 13:59:50 +0300

    Programing C#/C # Part II/04. Numeral-Systems-Homework added problem 2and 3

---
+++ Programing C#/C # Part II/04. Numeral-Systems-Homework/02. Binary-To-Decimal/BinarytoDecimal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class BinarytoDecimal
+{
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        Console.WriteLine(ConvertToDecimal(input));
+    }
+
+    private static long ConvertToDecimal(string binary)
+    {
+        long decimalNumber = 0;
+        for (int i = 0; i < binary.Length; i++)
+        {
+            long temp = binary[i] - '0';
+            int power = binary.Length - i - 1;
+            long powerOfTwo = temp << power;
+            decimalNumber += powerOfTwo * (binary[i] - '0');
+        }
+
+        return decimalNumber;
+    }
+}
---
+++ Programing C#/C # Part II/04. Numeral-Systems-Homework/03. Decimal-To-Hexadecimal/DecimalToHexadecimal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Decimal_To_Hexadecimal
+{
+    class DecimalToHexadecimal
+    {
+        static void Main(string[] args)
+        {
+            var num = long.Parse(Console.ReadLine());
+            var result = ConvertDecimalToAtherNumericSystem(num,16);
+            Console.WriteLine(result);
+        }
+
+        private static string ConvertDecimalToAtherNumericSystem(long number, int systemBase )
+        {
+            StringBuilder hexNumber = new StringBuilder();
+            while (number > 0)
+            {
+                var digit = number % systemBase;
+                if (digit < 10)
+                {
+                    hexNumber.Insert(0, digit);
+                }
+                else
+                {
+                    digit = 'A' + digit % 10;
+                    char hexValue = (char)digit;
+                    hexNumber.Insert(0, hexValue);
+                }
+                number = number / systemBase;
+            }
+
+            return hexNumber.ToString();
+        }
+    }
+}
