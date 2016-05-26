e39b3fbc9e0b062c71d5d92983607d86e5615adc
dushka-dragoeva <d.dragoeva@abv.bg>
2016-05-26 08:30:33 +0300

    Added new solution Extract sentences 80 points

---
+++ Programing C#/C # Part II/06. Strings-Text-Processing-Homework/08. Extract-Sentences2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+public class ExtractSentences
+{
+    public static void Main(string[] args)
+    {
+        var word = Console.ReadLine();
+        var text = Console.ReadLine();
+        Console.WriteLine(ExtratSentencesFromText(text, word));
+    }
+
+    private static string ExtratSentencesFromText(string text, string wordToFind)
+    {
+        var extractedText = new StringBuilder();
+
+        var allSentense = text.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Trim())
+            .ToArray();
+        for (int i = 0; i < allSentense.Length; i++)
+        {
+            if (FindWordInSentance(allSentense[i], wordToFind))
+            {
+                extractedText.Append(allSentense[i]);
+                extractedText.Append('.');
+
+                if (i < allSentense.Length)
+                {
+                    extractedText.Append(" ");
+                }
+            }
+        }
+
+        return extractedText.ToString().Trim();
+    }
+
+    private static bool FindWordInSentance(string sentance, string word)
+    {
+        bool isFind = false;
+        var nextIndexOfWord = sentance.IndexOf(word);
+
+        var lastIndex = sentance.Length - word.Length;
+
+        while (nextIndexOfWord > -1)
+        {
+            if (nextIndexOfWord != 0 && char.IsLetter(sentance[nextIndexOfWord - 1]))
+            {
+                nextIndexOfWord = sentance.IndexOf(word, nextIndexOfWord + 1);
+            }
+            else if (nextIndexOfWord < lastIndex && char.IsLetter(sentance[nextIndexOfWord + word.Length]))
+            {
+                nextIndexOfWord = sentance.IndexOf(word, nextIndexOfWord + 1);
+            }
+            else
+            {
+                isFind = true;
+                break;
+            }
+        }
+
+        return isFind;
+    }
+}
