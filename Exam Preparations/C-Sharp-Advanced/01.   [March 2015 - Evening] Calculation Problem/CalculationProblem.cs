c21ec76bbb609a80b27f8fedf98c24ab54d16467
dushka-dragoeva <d.dragoeva@abv.bg>
2016-05-26 13:42:50 +0300

    Added Exam Preparations/C-Sharp-Advanced March 2015 - Evening task 1 and 2

---
+++ Exam Preparations/C-Sharp-Advanced/01.   [March 2015 - Evening] Calculation Problem/CalculationProblem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+public class CalculationProblem
+{
+    private const int Base = 23;
+
+    public static void Main(string[] args)
+    {
+        var codedNumbers = Console.ReadLine()
+            .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+
+        BigInteger sum = 0;
+
+        foreach (var number in codedNumbers)
+        {
+            sum += NumberToDecimal(Base, number);
+        }
+
+        var encoded = DecimalToBaseN(sum, Base);
+        Console.WriteLine("{0} = {1}", encoded, sum);
+    }
+
+    private static BigInteger NumberToDecimal(byte baseA, string num)
+    {
+        BigInteger decimalNumber = 0;
+
+        for (int i = 0; i < num.Length; i++)
+        {
+            int digit = 0;
+            int position = num.Length - i - 1;
+
+            digit = num[i] - 'a';
+            decimalNumber = digit + (decimalNumber * baseA);
+        }
+
+        return decimalNumber;
+    }
+
+    private static string DecimalToBaseN(BigInteger decValue, int outputBase)
+    {
+        string result = string.Empty;
+
+        do
+        {
+            BigInteger reminder = decValue % outputBase;
+            char digitToChar = (char)(reminder + 'a');
+
+            ///  digit = num[i] - 'a';
+            result = digitToChar + result;
+            decValue /= outputBase;
+        }
+        while (decValue > 0);
+
+        return result;
+    }
+}
---
+++ Exam Preparations/C-Sharp-Advanced/02. [ March 2015 - Evening] Even Differences/EvenDifferences.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class EvenDifferences
+{
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine()
+            .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList<int>();
+
+        var evenDiffrencesSum = 0;
+
+        for (int i = 1; i < input.Count-1; i++)
+        {
+            var abcoluteDifference = Math.Abs(input[i] - input[i - 1]);
+            var step = 0;
+            if (abcoluteDifference % 2 == 0)
+            {
+                step = 2;
+                evenDiffrencesSum += abcoluteDifference;
+            }
+            else
+            {
+                step = 1;
+            }
+            if (i + step < input.Count)
+            {
+                Move(ref input, step, i);
+            }
+            else
+            {
+                continue;
+            }
+           Console.WriteLine(abcoluteDifference);
+           Console.WriteLine(string.Join(", ", input));
+        }
+
+        Console.WriteLine(evenDiffrencesSum);
+    }
+
+    private static void Move(ref List<int> numbers, int step, int index)
+    {
+        var current = numbers[index];
+        numbers.Remove(numbers[index]);
+        numbers.Insert(index + step, current);
+    }
+}
