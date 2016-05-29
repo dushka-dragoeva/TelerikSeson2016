5af079292e4a6743a0ed0388b5d913f7a9c3ac49
dushka-dragoeva <d.dragoeva@abv.bg>
2016-05-29 14:53:30 +0300

    IncreasingAbsoluteDifferences.cs added solution

---
+++ Exam Preparations/C-Sharp-Advanced/02. [ March06 2015 - Evening] IncreasingAbsDifference/IncreasingAbsoluteDifferences.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+public class IncreasingAbsoluteDifferences
+{
+    /// 1.Read input
+    /// 2. For Every sequence do:
+    /// 2.1 Calculate sequence of abs diff
+    /// 2.1 Check if they are increasing
+    /// 3. Output
+    public static void Main()
+    {
+        var rows = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < rows; i++)
+        {
+            var sequences = Console.ReadLine()
+           .Split(' ')
+           .Select(long.Parse)
+           .ToArray();
+
+            long last = sequences[i];
+            bool isEncreasing = true;
+
+            for (int j = 2; j < sequences.Length; j++)
+            {
+                var lastAbsDiff = Math.Abs(sequences[j - 1] - sequences[j - 2]);
+                var currAbsDiff = Math.Abs(sequences[j] - sequences[j - 1]);
+
+                if (lastAbsDiff > currAbsDiff)
+                {
+                    isEncreasing = false;
+                }
+            }
+
+            Console.WriteLine(isEncreasing);
+        }
+    }
+}
