a55ffebea1c8120f69aa8b284146b69fdd0ebd82
dushka-dragoeva <d.dragoeva@abv.bg>
2016-05-29 14:52:45 +0300

    EvenDifferences added description

--- Exam Preparations/C-Sharp-Advanced/02. [ March 2015 - Evening] Even Differences/EvenDifferences.cs
+++ Exam Preparations/C-Sharp-Advanced/02. [ March 2015 - Evening] Even Differences/EvenDifferences.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 public class EvenDifferences
 {
@@ -10,43 +7,21 @@ public class EvenDifferences
     {
         var input = Console.ReadLine()
             .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
-            .Select(int.Parse)
-            .ToList<int>();
+            .Select(long.Parse)
+            .ToArray();
 
-        var evenDiffrencesSum = 0;
+        long evenDiffrencesSum = 0;
 
-        for (int i = 1; i < input.Count-1; i++)
+        for (int i = 1; i < input.Length; i++)
         {
-            var abcoluteDifference = Math.Abs(input[i] - input[i - 1]);
-            var step = 0;
+           long abcoluteDifference = Math.Abs(input[i] - input[i - 1]);
             if (abcoluteDifference % 2 == 0)
             {
-                step = 2;
+                i++;
                 evenDiffrencesSum += abcoluteDifference;
             }
-            else
-            {
-                step = 1;
-            }
-            if (i + step < input.Count)
-            {
-                Move(ref input, step, i);
-            }
-            else
-            {
-                continue;
-            }
-           Console.WriteLine(abcoluteDifference);
-           Console.WriteLine(string.Join(", ", input));
         }
 
         Console.WriteLine(evenDiffrencesSum);
-    }
-
-    private static void Move(ref List<int> numbers, int step, int index)
-    {
-        var current = numbers[index];
-        numbers.Remove(numbers[index]);
-        numbers.Insert(index + step, current);
     }
 }
