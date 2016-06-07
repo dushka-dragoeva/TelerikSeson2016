421e5d702badab1bfa6085ed3bd58303a5f19142
dushka-dragoeva <d.dragoeva@abv.bg>
2016-06-07 17:00:52 +0300

    Added class GlobalConstants.cs

---
+++ Programing C#/OOP/01. Defining-Classes-Part-1/01.DefineClass/GlobalConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace MobileDevice
+{
+   public class GlobalConstants
+    {
+        public const string NoInformation = "no information";
+        public const string NullValue = " cannot be null";
+    }
+}
