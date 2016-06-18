50033a42917f7d601f66087a3087d0f0b1d8a7e4
dushka-dragoeva <d.dragoeva@abv.bg>
2016-06-18 08:21:10 +0300

    Extension methods Add task 17

---
+++ Programing C#/OOP/03. ExtensionMethodsDelegatesLambdaLINQ/17. LongestString/LongestStringTest.cs
@@ -0,0 +1,18 @@
+﻿namespace LongestString
+{
+    using System;
+    using StringExtentions;
+
+    public class LongestStringTest
+    {
+        public static void Run()
+        {
+            var words = new string[] { "lorem", "ipsum", "dolor", "sit", "amet", "consectetuer" };
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("17. Longest string in array");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(words.FindLongestString());
+        }
+    }
+}
---
+++ Programing C#/OOP/03. ExtensionMethodsDelegatesLambdaLINQ/17. LongestString/StringExtentions/StringFinder.cs
@@ -0,0 +1,12 @@
+﻿namespace LongestString.StringExtentions
+{
+    using System.Linq;
+
+    public static class StringFinder
+    {
+        public static string FindLongestString(this string[] words)
+        {
+            return words.OrderByDescending(w => w.Length).FirstOrDefault();
+        }
+    }
+}
