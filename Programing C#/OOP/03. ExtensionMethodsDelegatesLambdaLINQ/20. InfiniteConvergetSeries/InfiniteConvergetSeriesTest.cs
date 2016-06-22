09c11cfdd21b68301bdc6318b8a08863b37c79ff
dushka-dragoeva <d.dragoeva@abv.bg>
2016-06-22 08:35:14 +0300

    Programing C#/OOP/03. ExtensionMethodsDelegatesLambdaLINQ/20. InfiniteConvergetSeries added solution

---
+++ Programing C#/OOP/03. ExtensionMethodsDelegatesLambdaLINQ/20. InfiniteConvergetSeries/InfiniteConvergetSeriesTest.cs
@@ -0,0 +1,84 @@
+﻿namespace InfiniteConvergetSeries
+{
+    using System;
+
+    public class InfiniteConvergetSeriesTest
+    {
+        /// series 1/2^n
+        public static decimal NextMemberPowerOfTwo(ulong n)
+        {
+            ulong product = 1;
+
+            for (ulong i = 1; i <= n; i++)
+            {
+                checked
+                {
+                    product *= 2;
+                }
+            }
+
+            return ((decimal)1) / product;
+        }
+
+        public static decimal Error(decimal partialSum)
+        {
+            return (decimal)2 - partialSum;
+        }
+
+        /// series 1/n!
+        public static decimal FactorialSeriesNextMember(ulong n)
+        {
+            ulong product = 1;
+
+            for (ulong i = 1; i <= n + 1; i++)
+            {
+                checked
+                {
+                    product *= i;
+                }
+            }
+
+            return ((decimal)1) / product;
+        }
+
+        public static decimal FactorialError(decimal partialSum)
+        {
+            return (decimal)1.71828182845904523536028747135266249 - partialSum;
+        }
+
+        /// series 1 + ((-1)^(n+1))/2^n
+        public static decimal AlternativeSeriesNextMember(ulong n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                ulong product = 1;
+
+                for (ulong i = 0; i <= n; i++)
+                {
+                    product *= 2;
+                }
+
+                return (decimal)(n % 2 == 1 ? 1 : -1) / product;
+            }
+        }
+
+        public static decimal AlternativeSeriesError(decimal partialSum)
+        {
+            return Math.Abs((decimal)1.1666666666666666666666666666666666666666666666 - partialSum);
+        }
+
+        public static void Run()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("20. Infinite convergent series");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Sum of 1/2^n: {0:0.000}", SeriesCalculate.Calculate(NextMemberPowerOfTwo, Error));
+            Console.WriteLine("Sum of 1/n!: {0:0.000}", SeriesCalculate.Calculate(FactorialSeriesNextMember, FactorialError));
+            Console.WriteLine("Sum of 1 + 1/(-2)^(n+1): {0:0.000}", SeriesCalculate.Calculate(AlternativeSeriesNextMember, AlternativeSeriesError));
+        }
+    }
+}
---
+++ Programing C#/OOP/03. ExtensionMethodsDelegatesLambdaLINQ/20. InfiniteConvergetSeries/SeriesCalculate.cs
@@ -0,0 +1,29 @@
+﻿namespace InfiniteConvergetSeries
+{
+    using System;
+
+    public static class SeriesCalculate
+    {
+        public delegate decimal DenominatorFormula(ulong n); // will hold the formula for the n-th member of the series
+
+        public delegate decimal CalculateError(decimal partialSum); // will hold the formula for the error of the n-th partial sum of the series
+
+        /// METHODS
+        /// take a formula for the next member of the series, a formula for the error and a value for the precision
+        public static decimal Calculate(DenominatorFormula formula, CalculateError error, decimal prescision = (decimal)0.001)
+        {
+            ulong n = 0; /// start from the zero member
+            decimal sum = 0;
+
+            /// while the error is greater than we want it to be
+            while (error(sum) > prescision) 
+            {
+                sum += formula(n++); /// add the next member according to the formula we passes to it
+
+                Console.WriteLine(sum);
+            }
+
+            return sum;
+        }
+    }
+}
