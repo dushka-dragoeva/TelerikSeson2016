cf7be8fae9994a1bb5e4cb4218c197105de5938c
dushka-dragoeva <d.dragoeva@abv.bg>
2016-09-08 13:45:30 +0300

    High-Quality-Classes-Homework added

---
+++ Programing C#/High-Quality-Code-Part-One/08. High-Quality-Methods/Abstraction/Models/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using Abstraction.Contracts;
+
+namespace Abstraction
+{
+    internal class Rectangle : Figure, IFigure
+    {
+        public Rectangle(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Height { get; private set; }
+
+        public double Width { get; private set; }
+
+        public override double CalcPerimeter()
+        {
+            double perimeter = 2 * (this.Width + this.Height);
+            return perimeter;
+        }
+
+        public override double CalcSurface()
+        {
+            double surface = this.Width * this.Height;
+            return surface;
+        }
+    }
+}
