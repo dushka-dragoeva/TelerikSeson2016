7a16e87a92b2aeddbe6c12995f6187f1672194ed
dushka-dragoeva <d.dragoeva@abv.bg>
2017-02-13 14:04:00 +0200

    Renamed Data services to data Provider

---
+++ Software Technologies/WebForms/NewsSystem.Web/NewsSystem.Data.Services/ArticleDataProvider.cs
@@ -0,0 +1,60 @@
+﻿using NewsSystem.Data.Services.Contracts;
+using System.Linq;
+using NewsSystem.Data.Models;
+using NewsSystem.Data.Repositories;
+using System;
+
+namespace NewsSystem.Data.Services
+{
+    public class ArticleDataProvider : IArticleDataProvider
+    {
+        private IRepository<Article> articles;
+
+        public ArticleDataProvider(IRepository<Article> articles)
+        {
+            this.articles = articles;
+        }
+
+        public IQueryable<Article> GetAll()
+        {
+            return this.articles.All();
+        }
+
+        public IQueryable<Article> GetTop(int count)
+        {
+            return this.articles
+                .All()
+                .OrderByDescending(x => x.Likes.Count())
+                .Take(count);
+        }
+
+        public Article GetById(int id)
+        {
+            return this.articles.GetById(id);
+        }
+
+        public void Update(int id, Article updatedArticle)
+        {
+            var articleToUpdate = this.articles.GetById(id);
+
+            articleToUpdate.Title = updatedArticle.Title;
+            articleToUpdate.CategoryId = updatedArticle.CategoryId;
+            articleToUpdate.Content = updatedArticle.Content;
+
+           // this.articles.Update(articleToUpdate);
+            this.articles.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            this.articles.Delete(id);
+            this.articles.SaveChanges();
+        }
+
+        public void Create(Article article)
+        {
+            this.articles.Add(article);
+            this.articles.SaveChanges();
+        }
+    }
+}
---
+++ Software Technologies/WebForms/NewsSystem.Web/NewsSystem.Data.Services/Contracts/IArticleDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using NewsSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsSystem.Data.Services.Contracts
+{
+    public interface IArticleDataProvider
+    {
+        IQueryable<Article> GetTop(int count);
+
+        IQueryable<Article> GetAll();
+
+        Article GetById(int id);
+
+        void Update(int id, Article updatedArticle);
+
+        void Delete(int id);
+
+        void Create(Article article);
+
+    }
+}
